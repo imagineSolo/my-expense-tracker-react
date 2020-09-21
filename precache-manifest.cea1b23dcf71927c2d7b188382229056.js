d477e2d9e7bf4d81f4cb8c2ec7ed449c16913892
imagineSolo <soltom6@gmail.com>
2020-09-21 21:13:43 +0200

    Updates

---
+++ precache-manifest.cea1b23dcf71927c2d7b188382229056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185f7dcdd949ab9dcc73913cf58ff489",
+    "url": "/my-expense-tracker-react/index.html"
+  },
+  {
+    "revision": "04732a675a551cb13751",
+    "url": "/my-expense-tracker-react/static/css/main.4a29bb0c.chunk.css"
+  },
+  {
+    "revision": "8c018647778a674e10b8",
+    "url": "/my-expense-tracker-react/static/js/2.60cff590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-expense-tracker-react/static/js/2.60cff590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04732a675a551cb13751",
+    "url": "/my-expense-tracker-react/static/js/main.a9e56d7a.chunk.js"
+  },
+  {
+    "revision": "61b15359502f3f83a4df",
+    "url": "/my-expense-tracker-react/static/js/runtime-main.35bece39.js"
+  },
+  {
+    "revision": "5cc01d2c89693c970fb4288add8d6103",
+    "url": "/my-expense-tracker-react/static/media/trash.5cc01d2c.svg"
+  }
+]);
